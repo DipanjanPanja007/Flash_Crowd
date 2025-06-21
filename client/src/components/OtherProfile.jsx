82665592af42c7636b9eeadef4dc13110d5677b6
Ranit Jana <ranitjana100@gmail.com>
2025-06-22 01:12:34 +0530

    feat:other user info show

--- client/src/components/OtherProfile.jsx
+++ client/src/components/OtherProfile.jsx
@@ -23,6 +23,14 @@ const OtherProfile = ({ id }) => {
         getUser();
     }, [getUser])
 
+    if (!user) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <div className="text-gray-500">Loading user profile...</div>
+            </div>
+        );
+    }
+
     return (
 
         <div className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
