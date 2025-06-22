4b8d73d0a213df1d6fa9ed2edd4d29651872d0a4
Ranit Jana <ranitjana100@gmail.com>
2025-06-22 07:14:30 +0530

    feat:socket init

---
+++ client/src/context/SocketContext.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useEffect, useState } from "react";
+import { socket } from "../socket.js";
+import { useAuthContext } from "./AuthContext.jsx";
+
+const socketContext = createContext();
+
+export function useSocketContext() {
+  return useContext(socketContext);
+}
+
+//this context will hold the all games information that'll be shown in UI
+export default function SocketContext({ children }) {
+  const { playerInfo } = useAuthContext();
+  const [totalOnline, setTotalOnline] = useState(0);
+  const [onlineUsers, setOnlineUsers] = useState({});
+
+  useEffect(() => {
+    if (playerInfo) socket.emit("add-online-user", playerInfo._id);
+  }, [playerInfo]);
+
+  useEffect(() => {
+    const handleOnlineOfflineEvents = ({ onlineUsers }) => {
+      setTotalOnline(Object.keys(onlineUsers).length);
+      setOnlineUsers(onlineUsers);
+    };
+    socket.on("online-user", handleOnlineOfflineEvents);
+
+    return () => socket.off("online-user", handleOnlineOfflineEvents);
+  }, []);
+
+  return (
+    <socketContext.Provider value={{ totalOnline, onlineUsers }}>
+      {children}
+    </socketContext.Provider>
+  );
+}
